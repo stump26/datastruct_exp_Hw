eddaad9ed82aab7dd9d2eed298a80764680979a3
stump26 <moidpg@gmail.com>
2019-05-12 00:12:33 +0900

    btree 삽입 디버깅 완료

--- b_tree/b_tree/source.cpp
+++ b_tree/b_tree/source.cpp
@@ -1,3 +1,4 @@
+//input={7 20 3 12 11 8 9 13 17 19 14 1 5 10 6 2 15 18 16 21}
 #include<iostream>
 using namespace std;
 
@@ -5,76 +6,107 @@ const static int DEGREE=5;
 
 typedef struct _node {
 	int k[DEGREE - 1];
-	Node *son[DEGREE];
-	Node *father;
+	struct _node *son[DEGREE];
+	struct _node *father;
 	int numtree;
+	int numkey;
+	_node() {
+		for (int i = 0; i < DEGREE; i++) {
+			this->k[i] = 0;
+			this->son[i] = NULL;
+		}
+		this->son[DEGREE-1] = NULL;
+		this->father = NULL;
+		this->numtree = 0;
+		this->numkey = 0;
+	}
 }Node;
 class BTree {
 public:
-	Node *find();
-	Node *insleaf(int,Node*,int);
 	Node *makeTree(int);
-	Node *insnode(Node*, int, int, Node*);
-	void *split(Node*, int, int, Node*, Node*, int);
+	void insleaf(int,Node**);
+	void insnode(Node*, int, int, Node*);
+	void split(Node*, int, int, Node*, Node*, int*);
 	void copy(Node*, int, int, Node*);
 	int index(Node*);
+	int findpos(Node*,int);
+	Node *findnode(Node *,int);
 	BTree() {}
-	BTree(int key) {
-		makeTree(key);
-	}
 };
+void traverse(Node *);
+void node_print(Node *);
 
 int main() {
 	int input;
 	BTree tree;
-	Node* root; 
-	cout << "data>>";
-	cin >> input;
-	tree.insleaf(input, root, 0);
+	Node *root=NULL;
+	while (true) {
+		cout << "data>>";
+		cin >> input;
+		if (root == NULL) {
+			root = tree.makeTree(input);
+		}
+		else {
+			tree.insleaf(input, &root);
+		}
+		traverse(root);
+		cout << endl;
+	}
+
 	return 0;
 }
 void BTree::copy(Node *nd1, int first, int last, Node* nd2) {
-	nd1->numtree = last - first + 1;
-	for (int i = first; i <= last; i++) {
-		nd1->k[i] = nd2->k[i - first];
-		nd1->son[i] = nd2->son[i - first];
-		nd2->numtree++;
+	for (int i = 0; i <= last-first; i++) {
+		if (first + i <= nd1->numkey) {
+			nd2->k[i] = nd1->k[first + i];
+			nd1->k[first + i] = 0;
+
+			nd1->numkey--;
+			nd2->numkey++;
+		}
+		if (nd1->son[i+first]!=NULL) {
+			nd2->son[i] = nd1->son[i + first];
+			nd1->son[i + first] = NULL;
+
+			nd1->numtree--;
+			nd2->numtree++;
+		}
+		
 	}
 }
-void *BTree::split(Node* nd, int pos, int newkey, Node* newnode, Node* nd2, int midkey) {
-	nd2 = new Node;
-	nd2->numtree = 0;
+void BTree::split(Node* nd, int pos, int newkey, Node* newnode, Node* nd2, int *midkey){
 	int ndiv2 = DEGREE / 2;
 	if (pos > ndiv2) {        /* newkey 는 노드 node(nd2)에 속한다 */
-		copy(nd, ndiv2 + 1, DEGREE - 2, nd2);
+		copy(nd, ndiv2 + 1, DEGREE - 1, nd2);
 		insnode(nd2, pos - ndiv2 - 1, newkey, newnode);
-		nd->numtree = ndiv2 + 1;
-		midkey = nd->k[ndiv2];
+		*midkey = nd->k[ndiv2];
+		nd->k[ndiv2] = 0;
+		nd->numkey--;
 		return;
 	}
 	if (pos == ndiv2) {      /* newkey 가 중간 키가 된다. */
 		copy(nd, ndiv2, DEGREE - 2, nd2);
-		nd->numtree = ndiv2 + 1;
 		nd2->son[0] = newnode;
-		midkey = newkey;
+		*midkey = newkey;
 		return;
 	}
 	if (pos < ndiv2) {        /* newkey 는 노드 node(nd)에 속한다. */
 		copy(nd, ndiv2, DEGREE - 2, nd2);
-		nd->numtree = ndiv2;
 		insnode(nd, pos, newkey, newnode);
-		midkey = nd->k[ndiv2];
+		*midkey = nd->k[ndiv2];
+		nd->k[ndiv2] = 0;
+		nd->numkey--;
 		return;
 	}
 }
-Node *BTree::insnode(Node* nd, int pos, int newkey, Node* newnode) {
-	for (int i = nd->numtree - 1; i >= pos + 1; i--) {
+void BTree::insnode(Node* nd, int pos, int newkey, Node* newnode) {
+	for (int i = nd->numkey; i > pos; i--) {
 		nd->son[i+1] = nd->son[i];
 		nd->k[i] = nd->k[i-1];
 	}
 	nd->son[pos + 1] = newnode;
 	nd->k[pos] = newkey;
-	nd->numtree += 1;
+ 	nd->numkey++;
 }
 int BTree::index(Node *nd) {
 	for (int i = 0; i < nd->father->numtree; i++) {
@@ -83,34 +115,89 @@ int BTree::index(Node *nd) {
 	}
 	return -1;
 }
-Node *BTree::insleaf(int key, Node* s, int position) {
-	Node *tree;
-	Node *nd = s;
-	int pos = position;
+int BTree::findpos(Node *nd,int key) {
+	if (key < nd->k[0]) return 0;
+	for (int i = 0; i < nd->numkey; i++) {
+		if (key > nd->k[i] && key<nd->k[i+1]) {
+			return i+1;
+		}
+	}
+	return nd->numkey;
+}
+void BTree::insleaf(int key, Node** s){
+	Node *nd = findnode(*s,key);
 	Node *newnode = NULL;
 	int newkey = key;
 	Node *f = nd->father;
-	//Node *nd2;
-	while (f != NULL && nd->numtree == DEGREE) {
-		Node *nd2 =NULL;
-		int midkey = nd->k[nd->numtree / 2];
-		split(nd, pos, newkey, newnode,nd2, midkey);
+	//꽉찬 노드가 루트노드가 아닌지 확인
+	while (f != NULL && nd->numkey + 1 == DEGREE) {
+		Node *nd2 = new Node();
+		int midkey = nd->k[nd->numkey / 2];
+		split(nd, findpos(nd, key), newkey, newnode,nd2, &midkey);
+		f->numtree++;
+		nd2->father = f;
 		newnode = nd2;
-		pos = index(nd);
 		nd = f;
 		f = nd->father;
 		newkey = midkey;	
 	}
-	if (nd->numtree < DEGREE) {
-		insnode(nd, pos, newkey, newnode);
+	//노드가 꽉차지 않음
+	if (nd->numkey < DEGREE-1) {
+		insnode(nd, findpos(nd,key), newkey, newnode);
 		return;
 	}
-	Node *nd2 = NULL;
-	int midkey = nd->k[nd->numtree / 2];
-	split(nd, pos, newkey, newnode, nd2, midkey);
-	tree = new Node();
+	//루트노드가 꽉차 split 후 새 루트노드 생성
+	Node *nd2 = new Node();
+	int midkey = nd->k[nd->numkey / 2];
+	split(nd, findpos(nd, key), newkey, newnode, nd2, &midkey);
+	Node *tree = new Node();
 	tree->k[0] = midkey;
+	tree->numkey++;
+	nd->father = tree;
+	nd2->father = tree;
 	tree->son[0] = nd;
 	tree->son[1] = nd2;
-
+	tree->numtree = DEGREE / 2;
+	*s = tree;
+}
+Node *BTree::makeTree(int key) {
+	Node *nd = new Node();
+	nd->father = NULL;
+	nd->k[0] = key;
+	nd->numkey++;
+	return nd;
+}
+Node *BTree::findnode(Node *s,int key) {
+	Node * nd=s;
+	while (nd->numtree > 0) {
+		int pos = findpos(nd, key);
+		if (nd->son[pos] != NULL) {
+			nd = nd->son[pos];
+		}
+	}
+	return nd;
+}
+void traverse(Node *s) {
+	if (s->father == NULL) {
+		node_print(s);
+		cout << endl;
+	}
+	for (int i = 0; i < DEGREE; i++) {
+		if(s->son[i]!=NULL)
+			node_print(s->son[i]);
+	}
+	cout << endl;
+	for (int i = 0; i < DEGREE; i++) {
+		if (s->son[i] != NULL)
+			traverse(s->son[i]);
+	}
+}
+void node_print(Node *nd) {
+	cout << "{";
+	for (int i = 0; i < DEGREE-1; i++) {
+		if (nd!=NULL && nd->k[i] != 0){
+			cout << nd->k[i]<<", ";
+		}
+	}
+	cout << "}"<<nd->numtree;
 }
---
+++ balencedTree_second/balencedTree_second/balencedTree_second.cpp
@@ -0,0 +1,167 @@
+﻿//data:[58 43 27 13 21 18 35 38 41 32]
+
+#include<iostream>
+using namespace std;
+
+typedef struct _node {
+	int key;
+	struct _node *left;
+	struct _node *right;
+	int bal;
+}Node;
+Node * root;
+class Bal_Tree {
+public:
+	Node *maketree(int);
+	int height(Node*);
+	Node *checkbal(Node*);
+	Node *R_rotation(Node*);
+	Node *L_rotation(Node*);
+	Node *insert(Node*,int);
+	Node *balenced(Node*);
+	void inorder(Node*);
+	void preorder(Node*);
+	Bal_Tree() {
+	}
+};
+
+int main(void) {
+	int input;
+	Bal_Tree Tree;
+	
+	while (1) {
+		cout << "data>>";
+		cin >> input;
+		root = Tree.insert(root,input);
+		Tree.preorder(root);
+	}
+
+	return 0;
+}
+
+Node * Bal_Tree::insert(Node* root, int key) {
+	Node *q;
+	Node *fp = NULL;
+	Node *fya = NULL;
+	Node *ya = root;
+	Node *p = root;
+	if (root == NULL) {
+		q = maketree(key);
+		return q;
+	}
+	while (p != NULL) {
+		if (key == p->key)
+			return(p);
+		q = (key < p->key) ? p->left : p->right;
+		if (q != NULL)
+			if (q->bal != 0) {
+				fya = p;
+				ya = q;
+			}
+		fp = p;
+		p = q;
+	}
+	q = maketree(key);
+	q->bal = 0;
+	(key < fp->key) ? fp->left = q: fp->right = q;
+
+	checkbal(root);
+	root=balenced(root);
+	return root;
+}
+
+Node * Bal_Tree::maketree(int key) {
+	Node *q = new Node;
+	q->key = key;
+	q->bal = 0;
+	q->left=NULL;
+	q->right= NULL;
+	return q;
+}
+
+int Bal_Tree::height(Node* p) {
+	int h = 0;
+	if (p != NULL) {
+		int left = height(p->left);
+		int right = height(p->right);
+		int maxH = (left < right) ? right : left;
+		p->bal = left - right;
+		h = maxH+1;
+	}
+	return h;
+}
+Node *Bal_Tree::checkbal(Node *root) {
+	int left, right;
+	if (root != NULL) {
+		height(root);		
+	}
+	return root;
+}
+void Bal_Tree::inorder(Node *p) {
+	if (p == NULL)	return;
+	inorder(p->left);
+	cout << p->key<<"(" << p->bal << ")" << " ";
+	inorder(p->right);
+}
+void Bal_Tree::preorder(Node *p) {
+	if (p == NULL)
+		return;
+	cout << p->key << "(" << p->bal << ")" << " ";
+	preorder(p->left);
+	preorder(p->right);
+}
+
+Node *Bal_Tree::R_rotation(Node *p) {
+	Node *q,*hold;
+	q = p->left;
+	hold = q->right;
+	q->right = p;
+	p->left = hold;
+	return q;
+}
+Node *Bal_Tree::L_rotation(Node *p) {
+	Node *q, *hold;
+	q = p->right;
+	hold = q->left;
+	q->left = p;
+	p->right = hold;
+	return q;
+}
+Node *Bal_Tree::balenced(Node *p) {
+	if (p != NULL) {
+		p->left=balenced(p->left);
+		p->right=balenced(p->right);
+	}
+	else {
+		return p;
+	}
+	if (p->bal > 1)
+	{
+		if ((p->bal)-(p->left->bal)>2)
+		{
+			p->left = L_rotation(p->left);
+			p = R_rotation(p); 
+			checkbal(root);
+		}
+		else
+		{
+			p = R_rotation(p);
+			checkbal(root);
+		}
+	}
+	else if (p->bal < -1)
+	{
+		if ((p->bal) - (p->left->bal)<2)
+		{
+			p->right=R_rotation(p->right);
+			p=L_rotation(p);
+			checkbal(root);
+		}
+		else
+		{
+			p=L_rotation(p);
+			checkbal(root);
+		}
+	}
+	return p;
+}
