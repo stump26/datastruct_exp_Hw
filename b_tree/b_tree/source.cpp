98601ba7296d799a6dc59a79bceb106fbd6252b3
stump26 <moidpg@gmail.com>
2019-05-12 23:10:45 +0900

    삭제구현_디버깅필요

--- b_tree/b_tree/source.cpp
+++ b_tree/b_tree/source.cpp
@@ -1,5 +1,7 @@
 //input={7 20 3 12 11 8 9 13 17 19 14 1 5 10 6 2 15 18 16 21}
+//input={1 7 1 20 1 3 1 12 1 11 1 8 1 9 1 13 1 17 1 19 1 14 1 1 1 5 1 10 1 6 1 2 1 15 1 18 1 16 1 21}
 #include<iostream>
+#include<string>
 using namespace std;
 
 const static int DEGREE=5;
@@ -25,34 +27,68 @@ class BTree {
 public:
 	Node *makeTree(int);
 	void insleaf(int,Node**);
+	void delNode(int, Node**);
 	void insnode(Node*, int, int, Node*);
 	void split(Node*, int, int, Node*, Node*, int*);
 	void copy(Node*, int, int, Node*);
+	void delkey(Node*, int,int);
+	void mergeNode(Node*, Node*, int,int);
+	int check_numTree(Node*);
 	int index(Node*);
 	int findpos(Node*,int);
 	Node *findnode(Node *,int);
+	void traverse(Node*);
+	string node_print(Node*);
 	BTree() {}
 };
-void traverse(Node *);
-void node_print(Node *);
+
 
 int main() {
-	int input;
+	int input,mod;
 	BTree tree;
 	Node *root=NULL;
 	while (true) {
-		cout << "data>>";
-		cin >> input;
-		if (root == NULL) {
-			root = tree.makeTree(input);
-		}
-		else {
-			tree.insleaf(input, &root);
-		}
-		traverse(root);
-		cout << endl;
-	}
+		cout << "-----------" << endl;
+		cout << "1.삽입"<<endl;
+		cout << "2.삭제" << endl;
+		cout << "3.출력" << endl;
+		cout << "기능 선택 >> ";
+		cin >> mod;
 
+		switch (mod)
+		{
+		case 1:
+			cout << "data>>";
+			cin >> input;
+			if (root == NULL) {
+				root = tree.makeTree(input);
+			}
+			else {
+				tree.insleaf(input, &root);
+			}
+			break;
+		case 2:
+			cout << "data>>";
+			cin >> input;
+			if (root == NULL) {
+				cout << "ERROR!! empty tree" << endl;
+				break;
+			}
+			else {
+				tree.delNode(input, &root);
+			}
+			break;
+		case 3:
+			traverse(root);
+			cout << endl;
+			break;
+		default:
+			cout << "===!!오류!!====" << endl;
+			cout << "올바른 입력을 해주세요." << endl;
+			cout << "(1-3 내의 정수 입력)" << endl;
+			break;
+		}
+	}
 	return 0;
 }
 void BTree::copy(Node *nd1, int first, int last, Node* nd2) {
@@ -76,18 +112,23 @@ void BTree::copy(Node *nd1, int first, int last, Node* nd2) {
 }
 void BTree::split(Node* nd, int pos, int newkey, Node* newnode, Node* nd2, int *midkey){
 	int ndiv2 = DEGREE / 2;
+	
 	if (pos > ndiv2) {        /* newkey 는 노드 node(nd2)에 속한다 */
 		copy(nd, ndiv2 + 1, DEGREE - 1, nd2);
 		insnode(nd2, pos - ndiv2 - 1, newkey, newnode);
 		*midkey = nd->k[ndiv2];
 		nd->k[ndiv2] = 0;
 		nd->numkey--;
+		nd->numtree = check_numTree(nd);
+		nd2->numtree = check_numTree(nd2);
 		return;
 	}
 	if (pos == ndiv2) {      /* newkey 가 중간 키가 된다. */
 		copy(nd, ndiv2, DEGREE - 2, nd2);
 		nd2->son[0] = newnode;
 		*midkey = newkey;
+		nd->numtree = check_numTree(nd);
+		nd2->numtree = check_numTree(nd2);
 		return;
 	}
 	if (pos < ndiv2) {        /* newkey 는 노드 node(nd)에 속한다. */
@@ -96,6 +137,8 @@ void BTree::split(Node* nd, int pos, int newkey, Node* newnode, Node* nd2, int *
 		*midkey = nd->k[ndiv2];
 		nd->k[ndiv2] = 0;
 		nd->numkey--;
+		nd->numtree = check_numTree(nd);
+		nd2->numtree = check_numTree(nd2);
 		return;
 	}
 }
@@ -116,9 +159,9 @@ int BTree::index(Node *nd) {
 	return -1;
 }
 int BTree::findpos(Node *nd,int key) {
-	if (key < nd->k[0]) return 0;
+	if (key <= nd->k[0]) return 0;
 	for (int i = 0; i < nd->numkey; i++) {
-		if (key > nd->k[i] && key<nd->k[i+1]) {
+		if (key > nd->k[i] && key<=nd->k[i+1]) {
 			return i+1;
 		}
 	}
@@ -134,7 +177,7 @@ void BTree::insleaf(int key, Node** s){
 		Node *nd2 = new Node();
 		int midkey = nd->k[nd->numkey / 2];
 		split(nd, findpos(nd, key), newkey, newnode,nd2, &midkey);
-		f->numtree++;
+		f->numtree = check_numTree(f);
 		nd2->father = f;
 		newnode = nd2;
 		nd = f;
@@ -157,9 +200,10 @@ void BTree::insleaf(int key, Node** s){
 	nd2->father = tree;
 	tree->son[0] = nd;
 	tree->son[1] = nd2;
-	tree->numtree = DEGREE / 2;
+	tree->numtree = check_numTree(tree);
 	*s = tree;
 }
+
 Node *BTree::makeTree(int key) {
 	Node *nd = new Node();
 	nd->father = NULL;
@@ -177,6 +221,20 @@ Node *BTree::findnode(Node *s,int key) {
 	}
 	return nd;
 }
+void BTree::traverse(Node* s) {
+	string* result = new string[];
+	result[0] = node_print(s);
+	if (s->numtree != 0) {
+		for (int i = 0; i < s->numtree; i++) {
+			Node* nd = s->son[i];
+			while (nd->numtree != 0) {
+
+			}
+		}
+	}
+
+}
+/*
 void traverse(Node *s) {
 	if (s->father == NULL) {
 		node_print(s);
@@ -191,13 +249,111 @@ void traverse(Node *s) {
 		if (s->son[i] != NULL)
 			traverse(s->son[i]);
 	}
-}
-void node_print(Node *nd) {
-	cout << "{";
+}*/
+string BTree::node_print(Node *nd) {
+	string print="";
+	print += "{";
 	for (int i = 0; i < DEGREE-1; i++) {
 		if (nd!=NULL && nd->k[i] != 0){
-			cout << nd->k[i]<<", ";
+			print += nd->k[i]+", ";
 		}
 	}
-	cout << "}"<<nd->numtree;
+	print += "}";
+
+	return print;
+}
+void BTree::delNode(int key, Node** s) {
+	Node* nd = findnode(*s, key);
+	int pos = findpos(nd, key);
+	if (nd->k[pos] != key) {
+		cout << "Key(" << key << ") is not found." << endl;
+	}
+	else {
+		delkey(nd, key, pos);
+		if (nd->numkey >= DEGREE / 2) {//언더플로우 없는경우
+			return;
+		}
+		else {//언더플로우있는경우
+			int nd_pos;
+			Node* f=nd->father;
+			while (f!=NULL && nd->numkey <= DEGREE/2){
+				for (int i = 0; i < f->numtree; i++) {
+					if (nd == f->son[i]) { 
+						nd_pos = i; break; 
+					}
+				}
+				//numkey > n/2인 오른쪽 형제노드존재.
+				if (f->numtree - 1 > nd_pos && f->son[nd_pos + 1]->numkey > DEGREE / 2) {
+					int shift_key = f->son[nd_pos+1]->k[0];
+					delkey(f->son[nd_pos+1], shift_key, 0);
+						
+					int tmp = f->k[nd_pos];
+					f->k[nd_pos] = shift_key;
+					shift_key = tmp;
+
+					insnode(nd, nd->numkey, shift_key, NULL);
+				}
+				////numkey > n/2인 왼쪽 형제노드존재.
+				else if (nd_pos != 0 && f->son[nd_pos - 1]->numkey > DEGREE / 2) {
+					int shift_key = f->son[nd_pos - 1]->k[f->son[nd_pos - 1]->numkey-1];
+					delkey(f->son[nd_pos - 1], shift_key, f->son[nd_pos - 1]->numkey-1);
+
+					int tmp = f->k[nd_pos-1];
+					f->k[nd_pos-1] = shift_key;
+					shift_key = tmp;
+
+					insnode(nd, 0, shift_key, NULL);
+				}
+				//오른쪽 형제 노드 존재하지만 f->son[nd_pos + 1]->numkey<=n/2
+				else if (f->numtree - 1> nd_pos && f->son[nd_pos+1]->numkey<=DEGREE/2) {
+					mergeNode(nd, f->son[nd_pos + 1], f->k[nd_pos],nd_pos+1);
+					delkey(f, f->k[nd_pos], nd_pos);
+				}
+				//왼쪽형제 노드 존재하지만 f->son[nd_pos - 1]->numkey <= n/2 
+				else if (nd_pos != 0 && f->son[nd_pos-1]->numkey<=DEGREE/2) {
+					mergeNode(f->son[nd_pos-1],nd, f->k[nd_pos-1],nd_pos+1);
+					delkey(f, f->k[nd_pos-1], nd_pos-1);
+				}
+				nd = f;
+				f = nd->father;
+			}
+			if ((*s)->numkey==0) {
+				*s = (*s)->son[0];
+			}
+		}
+	}
+}
+void BTree::delkey(Node* nd, int key, int pos) {
+	int diff = nd->numkey - pos;
+	for (int i = 0; i < diff; i++) {
+		if (i + pos +1 < nd->numkey) {
+			nd->k[i + pos] = nd->k[i + pos + 1];
+		}
+		else {
+			nd->k[i + pos] = 0;
+		}
+	}
+	nd->numkey--;
+}
+void BTree::mergeNode(Node* nd, Node* nd2, int shiftkey,int pos) {
+	insnode(nd, nd->numkey, shiftkey, NULL);
+	for (int i = 0; i < nd2->numkey; i++) {
+		insnode(nd, nd->numkey, nd2->k[i], NULL);
+	}
+	for (int i = 0; i < nd2->numtree; i++) {
+		nd->son[nd->numtree] = nd2->son[i];
+		nd->numtree++;
+	}
+	for (int i = pos; i < nd->father->numtree-1;i++) {
+		nd->father->son[i] = nd->father->son[i + 1];
+	}
+	nd->father->numtree--;
+	nd->father->son[nd->father->numtree] = NULL;
+}
+int BTree::check_numTree(Node* nd){
+	int i = 0;
+	while (nd->son[i] != NULL) {
+		i++;
+	}
+	return i;
 }
